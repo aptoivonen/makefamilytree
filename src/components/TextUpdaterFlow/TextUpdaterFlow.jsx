8111b1e3ef1b800f7c6527f1c8315e19edec26e7
Aleksi Toivonen <aptoivonen@mail.com>
2022-06-21 14:54:40 +0300

    Add more stuff to TextUpdaterFlow

--- src/components/TextUpdaterFlow/TextUpdaterFlow.jsx
+++ src/components/TextUpdaterFlow/TextUpdaterFlow.jsx
@@ -17,6 +17,25 @@ const initialNodes = [
     position: { x: 0, y: 0 },
     data: { value: 123 },
   },
+  {
+    id: "node-2",
+    type: "output",
+    targetPosition: "top",
+    position: { x: 0, y: 200 },
+    data: { label: "node 2" },
+  },
+  {
+    id: "node-3",
+    type: "output",
+    targetPosition: "top",
+    position: { x: 200, y: 200 },
+    data: { label: "node 3" },
+  },
+];
+
+const initialEdges = [
+  { id: "edge-1", source: "node-1", target: "node-2", sourceHandle: "a" },
+  { id: "edge-2", source: "node-1", target: "node-3", sourceHandle: "b" },
 ];
 
 // we define the nodeTypes outside of the component to prevent re-renderings
@@ -25,7 +44,7 @@ const nodeTypes = { textUpdater: TextUpdaterNode };
 
 const TextUpdaterFlow = () => {
   const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState([]);
+  const [edges, setEdges] = useState(initialEdges);
 
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
--- src/components/TextUpdaterFlow/TextUpdaterNode.jsx
+++ src/components/TextUpdaterFlow/TextUpdaterNode.jsx
@@ -2,7 +2,8 @@ import { useCallback } from "react";
 import { Handle, Position } from "react-flow-renderer";
 import "./TextUpdaterNode.css";
 
-const handleStyle = { left: 10 };
+const leftHandleStyle = { left: 10 };
+const rightHandleStyle = { left: "calc(100% - 10px)" };
 
 const TextUpdaterNode = ({ data }) => {
   const onChange = useCallback((evt) => {
@@ -16,12 +17,18 @@ const TextUpdaterNode = ({ data }) => {
         <label htmlFor="text">Text</label>
         <input type="text" id="text" name="text" onChange={onChange} />
       </div>
-      <Handle type="source" position={Position.Bottom} id="a" />
       <Handle
         type="source"
         position={Position.Bottom}
-        id="b"
-        style={handleStyle}
+        id="a"
+        style={leftHandleStyle}
+      />
+      <Handle type="source" position={Position.Bottom} id="b" />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="c"
+        style={rightHandleStyle}
       />
     </div>
   );
