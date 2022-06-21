4e8a0ee56d614431939ff470dafb178c339f4656
Aleksi Toivonen <aptoivonen@mail.com>
2022-06-21 14:29:13 +0300

    Use TextUpdaterNode in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import { TestFlow } from "components";
+// import { TestFlow } from "components";
+import TextUpdaterFlow from "components/TextUpdaterFlow";
 
 function App() {
   return (
     <div className="App">
       <h1>Family Tree</h1>
       <div className="flow-container">
-        <TestFlow />
+        <TextUpdaterFlow />
       </div>
     </div>
   );
