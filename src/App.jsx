8e7b926c0e8d0c1201346cb551af2b3a019a0670
Aleksi Toivonen <aptoivonen@mail.com>
2022-06-22 16:16:32 +0300

    Create DagreLayoutFlow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
-import TextUpdaterFlow from "components/TextUpdaterFlow";
+// import TextUpdaterFlow from "components/TextUpdaterFlow";
 // import AddNewNodes from "components/AddNewNodes";
+import DagreLayoutFlow from "components/DagreLayoutFlow";
 
 function App() {
   return (
     <div className="App">
       <h1>Family Tree</h1>
       <div className="flow-container">
-        <TextUpdaterFlow />
+        <DagreLayoutFlow />
       </div>
     </div>
   );
