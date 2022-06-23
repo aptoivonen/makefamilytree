7add61c762ce2c00978ceb3e862857a4a467ebb1
Aleksi Toivonen <aptoivonen@mail.com>
2022-06-23 20:01:37 +0300

    Make node sizes constants

--- src/components/ReactFamilyTreeExample/ReactFamilyTreeExample.jsx
+++ src/components/ReactFamilyTreeExample/ReactFamilyTreeExample.jsx
@@ -5,8 +5,13 @@ import PersonNode from "./PersonNode";
 import nodes from "./nodes";
 import styles from "./ReactFamilyTreeExample.module.css";
 
-const personNodeWidthWithPadding = 180;
-const personNodeHeightWithPadding = 130;
+const PERSON_NODE_WIDTH = 150;
+const PERSON_NODE_HEIGHT = 70;
+const PERSON_NODE_HORIZONTAL_GAP = 30;
+const PERSON_NODE_VERTICAL_GAP = 60;
+
+const fullNodeWidth = PERSON_NODE_WIDTH + PERSON_NODE_HORIZONTAL_GAP;
+const fullNodeHeight = PERSON_NODE_HEIGHT + PERSON_NODE_VERTICAL_GAP;
 
 const ReactFamilyTreeExample = () => {
   const [rootId, setRootId] = useState(nodes[0].id);
@@ -16,19 +21,22 @@ const ReactFamilyTreeExample = () => {
       <ReactFamilyTree
         nodes={nodes}
         rootId={rootId}
-        width={personNodeWidthWithPadding}
-        height={personNodeHeightWithPadding}
+        width={fullNodeWidth}
+        height={fullNodeHeight}
         renderNode={(node) => (
           <div
             key={node.id}
             style={{
-              width: personNodeWidthWithPadding,
-              height: personNodeHeightWithPadding,
-              padding: "30px 15px",
+              width: fullNodeWidth,
+              height: fullNodeHeight,
+
+              padding: `calc(${PERSON_NODE_VERTICAL_GAP / 2}px) calc(${
+                PERSON_NODE_HORIZONTAL_GAP / 2
+              }px)`,
               position: "absolute",
-              transform: `translate(${
-                node.left * (personNodeWidthWithPadding / 2)
-              }px, ${node.top * (personNodeHeightWithPadding / 2)}px)`,
+              transform: `translate(${node.left * (fullNodeWidth / 2)}px, ${
+                node.top * (fullNodeHeight / 2)
+              }px)`,
             }}
           >
             <PersonNode
